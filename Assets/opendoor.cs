2d2b12a1a35642d0e4d19b9831bec9b9a7fe801c
Iluvsamsung <bys100100@gmail.com>
2020-11-15 20:44:44 +0900

    맵 크기 변경+문 스크립트 추가중
    문 스크립트는 아직 다 안만듬

---
+++ Assets/opendoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class opendoor : MonoBehaviour
+{
+
+
+
+    public Transform door;
+
+    void OnTriggerEnter(Collider coll)
+
+    {
+
+        if (coll.gameObject.tag == "student")
+
+        {
+
+            door.GetComponent<Animation>().Play("openthedoor");
+
+        }
+
+    }
+
+}
+
